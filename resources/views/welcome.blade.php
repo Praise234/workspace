f3c5f0e0865405b86be55c2fefcc184438f52b8e
Ayanwale Mofiyinfoluwa Praise <praise207@yahoo.com>
2023-06-02 11:50:40 +0100

    mail test 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -231,7 +231,8 @@
 
                 <div class="row align-items-center">
                     <div class="col-12 mt-4 pt-2">
-                        <form method="post" id="myForm" name="myForm" onsubmit="validateForm(event)">
+                        <form method="post" id="myForm" name="myForm" method="POST" action="{{Route('send_mail')}}">
+                            @csrf
                             <p class="mb-0" id="error-msg"></p>
                             <div id="simple-msg"></div>
                             <div class="row">
@@ -856,64 +857,6 @@
                 });
             }
         </script>
-        <script>
-            // Contact Form
-            function validateForm(e) {
-                e.preventDefault();
-                var name = document.forms["myForm"]["name"].value;
-                var email = document.forms["myForm"]["email"].value;
-                var subject = document.forms["myForm"]["subject"].value;
-                var comments = document.forms["myForm"]["comments"].value;
-                document.getElementById("error-msg").style.opacity = 0;
-                document.getElementById('error-msg').innerHTML = "";
-                if (name == "" || name == null) {
-                    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
-                    fadeIn();
-                    return false;
-                }
-                if (email == "" || email == null) {
-                    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
-                    fadeIn();
-                    return false;
-                }
-                if (subject == "" || subject == null) {
-                    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
-                    fadeIn();
-                    return false;
-                }
-                if (comments == "" || comments == null) {
-                    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
-                    fadeIn();
-                    return false;
-                }
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("simple-msg").innerHTML = this.responseText;
-                        document.forms["myForm"]["name"].value = "";
-                        document.forms["myForm"]["email"].value = "";
-                        document.forms["myForm"]["subject"].value = "";
-                        document.forms["myForm"]["comments"].value = "";
-                    }
-                };
-                xhttp.open("POST", "{{Route('send_mail')}}", true);
-                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xhttp.send("&_token=" + "{{csrf_token()}}" + "name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
-                return false;
-            }
-
-            function fadeIn() {
-                var fade = document.getElementById("error-msg");
-                var opacity = 0;
-                var intervalID = setInterval(function () {
-                    if (opacity < 1) {
-                        opacity = opacity + 0.5
-                        fade.style.opacity = opacity;
-                    } else {
-                        clearInterval(intervalID);
-                    }
-                }, 200);
-            }
-        </script>
+        
     </body>
 </html>
