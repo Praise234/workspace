b9feeb2170787d2a03b3d6d877e5671140d70970
Ayanwale Mofiyinfoluwa Praise <praise207@yahoo.com>
2023-07-06 15:56:42 +0100

    Edit Image 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
 	    <link href="css/style.min.css" rel="stylesheet" type="text/css" id="theme-opt" />
         <style>
             #imgg{
-                height: 400px;
+                height: 300px;
                 width: 300px;
                }
                #home{
